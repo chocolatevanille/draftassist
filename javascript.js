bec87631bc8877ca6151576c26224a603e4716bb
Kirzi <11909976+Kirzi@users.noreply.github.com>
2023-12-16 05:37:17 -0500

    nos only, just a few matchups so far

--- javascript.js
+++ javascript.js
@@ -1,11 +1,53 @@
+function venusaur() {
+	document.getElementById("venusaur").classList.add("draft-no");
+	document.getElementById("pikachu").classList.add("draft-no");
+	document.getElementById("ninetales").classList.add("draft-no");
+	document.getElementById("glaceon").classList.add("draft-no");
+}
+
 function blastoise() {
+	document.getElementById("blastoise").classList.add("draft-no");
 	document.getElementById("pikachu").classList.add("draft-no");
 	document.getElementById("ninetales").classList.add("draft-no");
 	document.getElementById("gardevoir").classList.add("draft-no");
-	document.getElementById("sylveon").classList.add("draft-yes");
 	document.getElementById("decidueye").classList.add("draft-no");
 	document.getElementById("espeon").classList.add("draft-no");
 	document.getElementById("delphox").classList.add("draft-no");
 	document.getElementById("dragapult").classList.add("draft-no");
 	document.getElementById("chandelure").classList.add("draft-no");
 }
+
+function buzzwole() {
+	document.getElementById("sylveon").classList.add("draft-no");
+}
+
+function umbreon() {
+	document.getElementById("cramorant").classList.add("draft-no");
+}
+
+function zoroark() {
+	document.getElementById("gardevoir").classList.add("draft-no");
+	document.getElementById("decidueye").classList.add("draft-no");
+	document.getElementById("espeon").classList.add("draft-no");
+	document.getElementById("delphox").classList.add("draft-no");
+	document.getElementById("dragapult").classList.add("draft-no");
+	document.getElementById("chandelure").classList.add("draft-no");
+}
+
+/*
+ 	document.getElementById("venusaur").classList.add("draft-");
+	document.getElementById("pikachu").classList.add("draft-");
+	document.getElementById("ninetales").classList.add("draft-");
+	document.getElementById("cramorant").classList.add("draft-");
+	document.getElementById("gardevoir").classList.add("draft-");
+	document.getElementById("sylveon").classList.add("draft-");
+	document.getElementById("decidueye").classList.add("draft-");
+	document.getElementById("espeon").classList.add("draft-");
+	document.getElementById("delphox").classList.add("draft-");
+	document.getElementById("glaceon").classList.add("draft-");
+	document.getElementById("mew").classList.add("draft-");
+	document.getElementById("dragapult").classList.add("draft-");
+	document.getElementById("chandelure").classList.add("draft-");
+	document.getElementById("inteleon").classList.add("draft-");
+	document.getElementById("mewtwo-y").classList.add("draft-");
+*/
