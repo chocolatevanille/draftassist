91fe053a2d88b8fb3d94212f4e840862c3b14e56
Kirzi <11909976+Kirzi@users.noreply.github.com>
2023-12-16 04:44:29 -0500

    first

---
+++ javascript.js
@@ -0,0 +1,11 @@
+function blastoise() {
+	document.getElementById("pikachu").classList.add("draft-no");
+	document.getElementById("ninetales").classList.add("draft-no");
+	document.getElementById("gardevoir").classList.add("draft-no");
+	document.getElementById("sylveon").classList.add("draft-yes");
+	document.getElementById("decidueye").classList.add("draft-no");
+	document.getElementById("espeon").classList.add("draft-no");
+	document.getElementById("delphox").classList.add("draft-no");
+	document.getElementById("dragapult").classList.add("draft-no");
+	document.getElementById("chandelure").classList.add("draft-no");
+}
